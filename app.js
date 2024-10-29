55752674c2e0d0e0f6c097302cbf610012d18ce4
SameeJay <127780118+SameeJay@users.noreply.github.com>
2024-10-29 15:21:48 +0530

    weather API

--- app.js
+++ app.js
@@ -27,5 +27,5 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log(`Server is running at http://localhost:3000`);
+  console.log(`Server is running: http://localhost:3000`);
 });
